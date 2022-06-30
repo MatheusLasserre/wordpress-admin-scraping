2ed529c9bf82983b15a6318632a860881da75367
Matheus Lasserre <matheuslasserre3@gmail.com>
2022-06-30 02:58:21 -0300

    Updated Scrape.js comment to be more beginner friendly

--- Scrape.js
+++ Scrape.js
@@ -46,16 +46,18 @@ async function Scrape() {
     ]);
     console.log('Logged in.')
 
-    // going to subscribed users page
+    // just to know when the loop  will start
     console.log('Going to subscribed users page & Starting Search through all pages...')
 
-    // Loop throught all available pages
+    // Loop throught all available pages --- I used fixed number of loops because i didn't have any reason to not
     for (let i = 1; i <= 47; i++) {
         console.log(`For page ${i}:`)
+        // going to the page that have the info i need to reach the loop of pages that has the user information
         await page.goto(`https:your-site.com/wp-admin/users.php?filter_group_ids%5B0%5D=2&paged=${i.toString()}`, { waitUntil: 'networkidle0' });
 
         // get All users id from this page
         console.log('Getting all users id from page...');
+        // inside evaluate you can use any Js as if you were on the page yourself
         const idObject = await page.evaluate(() => {
             let tableIds = document.querySelector('.wp-list-table').tBodies[0].rows;
             const idArray = [];
@@ -70,14 +72,15 @@ async function Scrape() {
         console.log(!!idObject)
         console.log('Done.')
 
-        // Looping through users of page 
+        // Looping through page of each user
         console.log('Getting users info from page...');
 
         for (let x = 0; x < idObject.length; x++) {
             await page.goto(`https://your-site/wp-admin/user-edit.php?user_id=${idObject[x].toString()}&wp_http_referer=%2Fwp-admin%2Fusers.php%3Ffilter_group_ids%255B%255D%3D2`, {waitUntil: 'networkidle0'});
 
 
-            // Thats the logic i used for the info i needed
+            // Thats the logic i used for the info i needed -- you probably CAN'T use the same as i did.
+            // But inside page.evaluate it's just  Js, you probably can do your own easily
             userObject = await page.evaluate(() => {
                 let email = document.getElementById('email').value;
                 let firstname = email.substring(0, email.indexOf('@'));
@@ -92,12 +95,12 @@ async function Scrape() {
                 return dumbObject
             })
             usersArray.push(userObject);
-            // Simple one-line terminal progress counter
-            process.stdout.write("\r\x1b[K");
+            // Simple one-line terminal progress counter 
+            process.stdout.write("\r\x1b[K"); // This line just remove the previous CLI message e.g: remove 2/15 at the same time that 3/15 is printed and so on
             process.stdout.write(`${x + 1}/${idObject.length }`);
 
         }
-        console.log(' ');
+        console.log(' '); // this line just remove a cli visual bug because i mixed process. with console.log
         console.log('Done.');
         console.log(`Finished page ${i}`);
     }
